0779168cc959b7b9ac5e80b1f20d0ea2d802e200
Francesco010703 <118008638+Francesco010703@users.noreply.github.com>
2022-11-11 19:21:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+  //Qui vanno definite le variabili
+  float a=0;                      //inizializzo le variabili delle misure
+  float b=0;
+  float c=0;
 
-cin >> a >> b >> c;
+  cin >> a >> b >> c;             //l'utente inserisce le varie misure 
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+  //Qui va scritto il codice per eseguire i calcoli richiesti
 
+  cout << (a*b)/2 << endl;        //Stampo a video l'area del triangolo
+  cout << a*a << endl;            //Stampo a video l'area del quadrato
+  cout << a*b << endl;            //Stampo a video l'area del rettangolo
+  cout << ((a+b)*c)/2 << endl;    //Stampo a video l'area del trapezio
 
 }
